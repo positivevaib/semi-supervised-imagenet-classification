2b254fa927e1a35e0f72dd2c8752fd2d0fa16017
positivevaib <vaib.gadodia@gmail.com>
2019-04-19 18:41:23 -0400

    Replace interpolation layers with transpose convolution

--- conv-autoencoder/dnn.py
+++ conv-autoencoder/dnn.py
@@ -3,31 +3,32 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# define classes
+# define class
 class Net(nn.Module):
     '''conv. autoencoder'''
     def __init__(self):
         '''constructor'''
         super().__init__()
 
-        self.enc1 = nn.Conv2d(3, 32, 5, padding = 2)
-        self.enc2 = nn.Conv2d(32, 64, 3, padding = 1)
-        self.enc3 = nn.Conv2d(64, 128, 3, padding = 1)
+        self.conv1 = nn.Conv2d(3, 32, 5, padding = 2)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding = 1)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding = 1)
 
-        self.dec1 = nn.Conv2d(128, 64, 3, padding = 1)
-        self.dec2 = nn.Conv2d(64, 32, 3, padding = 1)
-        self.dec3 = nn.Conv2d(32, 3, 3, padding = 1)
-        self.dec4 = nn.Conv2d(3, 3, 5, padding = 2)
+        self.deconv1 = nn.ConvTranspose2d(128, 64, 2, stride = 2)
+        self.deconv2 = nn.ConvTranspose2d(64, 32, 2, stride = 2)
+        self.deconv3 = nn.ConvTranspose2d(32, 3, 2, stride = 2)
+
+        self.conv4 = nn.Conv2d(3, 3, 5, padding = 2)
 
     def forward(self, x):
         '''forward prop.'''
-        x = F.max_pool2d(F.relu(self.enc1(x)), 2)
-        x = F.max_pool2d(F.relu(self.enc2(x)), 2)
-        x = F.max_pool2d(F.relu(self.enc3(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv3(x)), 2)
 
-        x = F.interpolate(F.relu(self.dec1(x)), scale_factor = 2, mode = 'bilinear', align_corners = False)
-        x = F.interpolate(F.relu(self.dec2(x)), scale_factor = 2, mode = 'bilinear', align_corners = False)
-        x = F.interpolate(F.relu(self.dec3(x)), scale_factor = 2, mode = 'bilinear', align_corners = False)
-        x = torch.sigmoid(self.dec4(x))
+        x = F.relu(self.deconv1(x))
+        x = F.relu(self.deconv2(x))
+        x = F.relu(self.deconv3(x))
+        x = torch.sigmoid(self.conv4(x))
 
         return x
