f6243a1340c71ac7dd5d058c074ec26f89d7246e
positivevaib <vaib.gadodia@gmail.com>
2019-04-27 17:42:49 -0400

    Add arguments

--- rotation-net/main.py
+++ rotation-net/main.py
@@ -16,28 +16,14 @@ parser.add_argument('--architecture',
                     type=str,
                     default='alexnet',
                     help='type of model architecture to use')
-parser.add_argument('--checkpoint',
-                    type=str,
-                    default=None,
-                    help='absolute path to model checkpoint')
-parser.add_argument('--data',
-                    type=str,
-                    default=None,
-                    help='absolute path to datasets')
 parser.add_argument('--environment',
                     type=str,
                     default='semi-supervised-imagenet-classification',
                     help='visdom environment name')
-parser.add_argument('--evaluate',
-                    action='store_true',
-                    help='evaluate model performance on test set')
 parser.add_argument('--learning_rate',
                     type=float,
                     default=0.001,
                     help='initial learning rate')
-parser.add_argument('--load',
-                    action='store_true',
-                    help='load pre-trained model')
 parser.add_argument('--matplotlib',
                     action='store_true',
                     help='display matplotlib figure')
@@ -51,6 +37,14 @@ parser.add_argument('--resume',
 parser.add_argument('--self_supervised',
                     action='store_true',
                     help='perform self-supervised training or evaluation')
+parser.add_argument('--self_supervised_checkpoint',
+                    type=str,
+                    default=None,
+                    help='absolute path to self_supervised model checkpoint')
+parser.add_argument('--self_supervised_data',
+                    type=str,
+                    default=None,
+                    help='absolute path to self-supervised dataset')
 parser.add_argument('--self_supervised_epochs',
                     type=int,
                     default=100,
@@ -59,17 +53,28 @@ parser.add_argument('--self_supervised_history',
                     type=str,
                     default=None,
                     help='absolute path to self-supervised training history')
+parser.add_argument('--self_supervised_load',
+                    action='store_true',
+                    help='load pre-trained self-supervised model')
 parser.add_argument('--self_supervised_model',
                     type=str,
                     default=None,
                     help='absolute path to save or load self-supervised model')
-parser.add_argument('--split',
+parser.add_argument('--self_supervised_split',
                     type=float,
                     default=0.8,
-                    help='training and validation split ratio')
+                    help='self_supervised training and validation split ratio')
 parser.add_argument('--supervised',
                     action='store_true',
                     help='perform supervised training or evaluation')
+parser.add_argument('--supervised_checkpoint',
+                    type=str,
+                    default=None,
+                    help='absolute path to supervised model checkpoint')
+parser.add_argument('--supervised_data',
+                    type=str,
+                    default=None,
+                    help='absolute path to supervised dataset')
 parser.add_argument('--supervised_epochs',
                     type=int,
                     default=100,
@@ -78,10 +83,25 @@ parser.add_argument('--supervised_history',
                     type=str,
                     default=None,
                     help='absolute path to supervised training history')
+parser.add_argument('--supervised_load',
+                    action='store_true',
+                    help='load pre-trained supervised model')
 parser.add_argument('--supervised_model',
                     type=str,
                     default=None,
                     help='absolute path to save or load supervised model')
+parser.add_argument('--supervised_split',
+                    type=float,
+                    default=0.8,
+                    help='supervised training and validation split ratio')
+parser.add_argument('--test',
+                    action='store_true',
+                    help='evaluate model performance on test set')
+parser.add_argument('--test_data',
+                    type=str,
+                    default=None,
+                    help='absolute path to test dataset')
+parser.add_argument('--train', action='store_true', help='train model')
 parser.add_argument('--train_batch',
                     type=int,
                     default=128,
@@ -120,31 +140,32 @@ model.to(device)
 criterion = nn.CrossEntropyLoss()
 
 # train model
-if not args.evaluate:
-    print('training model')
-
-    # instantiate dataloaders
-    train_loader, val_loader = data.get_data_loaders(
-        args.data,
-        train_ratio=args.split,
-        train_batch_size=args.train_batch,
-        val_batch_size=args.val_batch)
-
-    train_size = len(train_loader) * args.train_batch
-    val_size = len(val_loader) * args.val_batch
-
-    print('train set: {} images'.format(train_size))
-    print('val set: {} images\n'.format(val_size))
-
-    # instantiate optimizer and learning rate scheduler
-    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3)
+if args.train:
+    print('training model\n')
 
     # perform self-supervised training
     if args.self_supervised:
         print('self-supervised training\n')
+
+        # instantiate dataloaders
+        train_loader, val_loader = data.get_data_loaders(
+            args.self_supervised_data,
+            train_ratio=args.self_supervised_split,
+            train_batch_size=args.train_batch,
+            val_batch_size=args.val_batch)
+
+        train_size = len(train_loader) * args.train_batch
+        val_size = len(val_loader) * args.val_batch
+
+        print('train set: {} images'.format(train_size))
+        print('val set: {} images\n'.format(val_size))
+
+        # instantiate optimizer and learning rate scheduler
+        optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3)
+
         # load pre-trained model
-        if args.load:
+        if args.self_supervised_load:
             print('loading pre-trained model\n')
             model.load_state_dict(
                 torch.load(args.self_supervised_model, map_location=device))
@@ -153,13 +174,32 @@ if not args.evaluate:
         train.train(model, train_loader, train_size, val_loader, val_size,
                     criterion, optimizer, scheduler,
                     args.self_supervised_epochs, device,
-                    args.self_supervised_model, args.checkpoint,
+                    args.self_supervised_model,
+                    args.self_supervised_checkpoint,
                     args.self_supervised_history, args.resume, args.visdom,
                     args.environment, args.matplotlib, args.pbar_file)
 
     # perform supervised training
     if args.supervised:
         print('supervised training\n')
+
+        # instantiate dataloaders
+        train_loader, val_loader = data.get_data_loaders(
+            args.supervised_data,
+            train_ratio=args.supervised_split,
+            train_batch_size=args.train_batch,
+            val_batch_size=args.val_batch)
+
+        train_size = len(train_loader) * args.train_batch
+        val_size = len(val_loader) * args.val_batch
+
+        print('train set: {} images'.format(train_size))
+        print('val set: {} images\n'.format(val_size))
+
+        # instantiate optimizer and learning rate scheduler
+        optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3)
+
         # prepare model for transfer learning
         for param in model.parameters():
             param.require_grad = False
@@ -175,7 +215,7 @@ if not args.evaluate:
             model.out = nn.Linear(2048, 1000).to(device)
 
         # load pre-trained model
-        if args.load:
+        if args.supervised_load:
             print('loading pre-trained model\n')
             model.load_state_dict(
                 torch.load(args.supervised_model, map_location=device))
@@ -183,36 +223,38 @@ if not args.evaluate:
         # train
         train.train(model, train_loader, train_size, val_loader, val_size,
                     criterion, optimizer, scheduler, args.supervised_epochs,
-                    device, args.supervised_model, args.checkpoint,
+                    device, args.supervised_model, args.supervised_checkpoint,
                     args.supervised_history, args.resume, args.visdom,
                     args.environment, args.matplotlib, args.pbar_file)
 
-# evaluate model
+# test model
 else:
     print('evaluating model\n')
 
-    # load pre-trained model
-    if args.load:
-        print('loading pre-trained model\n')
-        if args.supervised:
-            if args.architecture == 'alexnet':
-                model.out = nn.Linear(4096, 1000).to(device)
-            elif args.architecture == 'resnet18' or \
-                 args.architecture == 'resnet34':
-                model.out = nn.Linear(512, 1000).to(device)
-            elif args.architecture == 'resnet50' or \
-                 args.architecture == 'resnet101' or \
-                 args.architecture == 'resnet152':
-                model.out = nn.Linear(2048, 1000).to(device)
+    # prepare model
+    if args.supervised:
+        if args.architecture == 'alexnet':
+            model.out = nn.Linear(4096, 1000).to(device)
+        elif args.architecture == 'resnet18' or \
+                args.architecture == 'resnet34':
+            model.out = nn.Linear(512, 1000).to(device)
+        elif args.architecture == 'resnet50' or \
+                args.architecture == 'resnet101' or \
+                args.architecture == 'resnet152':
+            model.out = nn.Linear(2048, 1000).to(device)
 
-            model.load_state_dict(
-                torch.load(args.supervised_model, map_location=device))
-        elif args.self_supervised:
-            model.load_state_dict(
-                torch.load(args.self_supervised_model, map_location=device))
+    # load pre-trained model
+    if args.self_supervised_load:
+        print('loading pre-trained model\n')
+        model.load_state_dict(
+            torch.load(args.self_supervised_model, map_location=device))
+    elif args.supervised_load:
+        print('loading pre-trained model\n')
+        model.load_state_dict(
+            torch.load(args.supervised_model, map_location=device))
 
     # instantiate dataloader
-    dataloader = data.get_data_loaders(args.data,
+    dataloader = data.get_data_loaders(args.test_data,
                                        test_loader=True,
                                        test_batch_size=args.val_batch)
     data_size = len(dataloader) * args.val_batch
